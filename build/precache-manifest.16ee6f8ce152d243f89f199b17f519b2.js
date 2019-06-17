6d51849355818a2bc220edb15ac517d59c0e8ffb
Sambo Chea <sombochea100@gmail.com>
2019-06-17 09:21:04 +0700

    add build

---
+++ build/precache-manifest.16ee6f8ce152d243f89f199b17f519b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc10821383fa3a7ea2529174d6ba48d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f25f71df32a59f890528",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3ec9d897e7bd85dfdae4",
+    "url": "/static/js/2.59e7c961.chunk.js"
+  },
+  {
+    "revision": "f25f71df32a59f890528",
+    "url": "/static/js/main.ffc2c001.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
